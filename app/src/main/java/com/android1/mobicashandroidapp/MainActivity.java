7746bf78d8e054f1ea1c1bdcdbdcbb5e751fc0f1
Eliane-5 <irankundaeliane9@gmail.com>
2020-01-28 16:52:21 +0200

    retrieving user information from firebase

--- app/src/main/java/com/android1/mobicashandroidapp/MainActivity.java
+++ app/src/main/java/com/android1/mobicashandroidapp/MainActivity.java
@@ -7,15 +7,52 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView fullName, email, phone,gender;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullName = findViewById(R.id.name);
+        phone = findViewById(R.id.mPhone);
+        email = findViewById(R.id.mEmail);
+        gender = findViewById(R.id.gender);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                phone.setText(documentSnapshot.getString("phone"));
+                fullName.setText(documentSnapshot.getString("fName"));
+                email.setText(documentSnapshot.getString("email"));
+                gender.setText(documentSnapshot.getString("gender"));
+            }
+        });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
