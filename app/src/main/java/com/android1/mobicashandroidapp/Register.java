fe524d9dd19ce23c96b0bc08cf182206d5409a24
Eliane-5 <irankundaeliane9@gmail.com>
2020-01-28 14:56:11 +0200

    added saving data into firebase functionality

--- app/src/main/java/com/android1/mobicashandroidapp/Register.java
+++ app/src/main/java/com/android1/mobicashandroidapp/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    EditText mFullName, mEmail, mPassword,mPhone;
+    public static final String TAG = "TAG";
+    EditText mFullName, mEmail, mPassword,mPhone, mGender;
     Button mRegisterButton;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +49,10 @@ public class Register extends AppCompatActivity {
         mPhone = findViewById(R.id.phone);
         mRegisterButton = findViewById(R.id.button);
         mLoginBtn = findViewById(R.id.createText);
+        mGender = findViewById(R.id.gender);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 //      progressBar = findViewById(R.id.progressBar);
 
         if (fAuth.getCurrentUser()!=null){
@@ -57,8 +71,12 @@ public class Register extends AppCompatActivity {
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String phone = mPhone.getText().toString();
+                final String gender = mGender.getText().toString();
+
                 if (TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
                 }
@@ -75,6 +93,25 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(Register.this,"user created.", Toast.LENGTH_SHORT).show();
+                            userId = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userId);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName",fullName);
+                            user.put("email",email);
+                            user.put("phone",phone);
+                            user.put("gender",gender);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG,"successful: user profile is created for: "+userId);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: "+ e.toString());
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else {
                             Toast.makeText(Register.this,"there has been an error."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
